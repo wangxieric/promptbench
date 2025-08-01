806bd12358b223ac9352298c4bd47202b44bd2e8
Xi Wang <wangxieric@gmail.com>
2025-08-01 14:39:45 +0100

    Update models.py

--- promptbench/models/models.py
+++ promptbench/models/models.py
@@ -277,7 +277,7 @@ class CogBiasModel(LMMBaseModel):
         The dtype to use for inference (default is 'auto').
     """
     def __init__(self, model_name, max_new_tokens, temperature, device, dtype, system_prompt=None):
-        super(LlamaModel, self).__init__(model_name, max_new_tokens, temperature, device)
+        super(CogBiasModel, self).__init__(model_name, max_new_tokens, temperature, device)
         if system_prompt is None:
             self.system_prompt = "You are a helpful, respectful and honest assistant. Always answer as helpfully as possible, while being safe.  Your answers should not include any harmful, unethical, racist, sexist, toxic, dangerous, or illegal content. Please ensure that your responses are socially unbiased and positive in nature.\nIf a question does not make any sense, or is not factually coherent, explain why instead of answering something not correct. If you don't know the answer to a question, please don't share false information."
         else:
