d56e9692a8a2850a1f796fc684ecc6c7bdb102e1
Xi Wang <wangxieric@gmail.com>
2025-08-04 11:51:28 +0100

    Update eval.py

--- eval.py
+++ eval.py
@@ -6,39 +6,43 @@ dataset = pb.DatasetLoader.load_dataset("sst2")
 print("dataset loaded:", dataset[:5])
 
 model = pb.LLMModel("XiWangEric/literary-classicist-llama3", max_new_tokens=10, temperature=0.0001, device='cuda')
-prompt = "Determine the emotion of the following sentence as positive or negative: looking aristocratic , luminous yet careworn in jane hamilton 's exemplary costumes , rampling gives a performance that could not be improved upon . '"  # simple prefix prompt
+prompt = "Once upon a time, in a quiet village,"  # simple prefix prompt
 outputs = model(prompt)
 
 print("=== Generated Text ===")
 print(outputs)
 
+prompt = "classify the sentiment of the following sentence: 'I love programming!'"
+outputs = model(prompt)
+print("=== Generated Text ===")
+print(outputs)
 
 prompts = pb.Prompt(["Classify the sentence as positive or negative: {content}",
                      "Determine the emotion of the following sentence as positive or negative: {content}"
                      ])
 
-def proj_func(pred):
-    mapping = {
-        "positive": 1,
-        "negative": 0
-    }
-    return mapping.get(pred, -1)
+# def proj_func(pred):
+#     mapping = {
+#         "positive": 1,
+#         "negative": 0
+#     }
+#     return mapping.get(pred, -1)
 
-from tqdm import tqdm
-for prompt in prompts:
-    preds = []
-    labels = []
-    for data in tqdm(dataset):
-        # process input
-        input_text = pb.InputProcess.basic_format(prompt, data)
-        # print(f"input_text: {input_text}")
-        label = data['label']
-        raw_pred = model(input_text)
-        # process output
-        pred = pb.OutputProcess.cls(raw_pred, proj_func)
-        preds.append(pred)
-        labels.append(label)
+# from tqdm import tqdm
+# for prompt in prompts:
+#     preds = []
+#     labels = []
+#     for data in tqdm(dataset):
+#         # process input
+#         input_text = pb.InputProcess.basic_format(prompt, data)
+#         print(f"input_text: {input_text}")
+#         label = data['label']
+#         raw_pred = model(input_text)
+#         # process output
+#         pred = pb.OutputProcess.cls(raw_pred, proj_func)
+#         preds.append(pred)
+#         labels.append(label)
     
-    # evaluate
-    score = pb.Eval.compute_cls_accuracy(preds, labels)
-    print(f"{score:.3f}, {prompt}")
+#     # evaluate
+#     score = pb.Eval.compute_cls_accuracy(preds, labels)
+#     print(f"{score:.3f}, {prompt}")
